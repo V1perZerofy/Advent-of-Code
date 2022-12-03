bc402de1c9f29433db4f84f6e44a5d2c336628ec
V1perZerofy <niels.heiden@t-online.de>
2022-12-03 15:17:12 +0100

    Day 1 solved

---
+++ 2022/Day 1/Day1.py
@@ -0,0 +1,13 @@
+with open("input.txt") as file:
+    data = list(map(int, file.readlines()))   
+calories = 0
+inv = []
+for i in range(len(data)):
+    if data[i] != 0:
+        calories += data[i]
+    else:
+        inv.append(calories)
+        calories = 0
+inv.sort(reverse = True | False)
+print("The biggest is: " + str(inv[0]))
+print("The sum of the three biggest is: " + str(inv[0] + inv[1] + inv[2]))
